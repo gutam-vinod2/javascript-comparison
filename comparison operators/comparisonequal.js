a275d5d50a9fa069eb2783d21d749ca49b7c8c20
[firstname lastname] <[valid-email]>
2022-08-16 19:09:09 +0530

    comparison

---
+++ comparison operators/comparisonequal.js
@@ -0,0 +1,60 @@
+equal
+var a = 5;
+var b = 10;
+var c = 5;
+var x = (a==c);
+console.log(x)
+
+unequal
+var a = 5;
+var b = 10;
+var c = 5;
+var x = (a===c);
+console.log(x)
+
+
+var a = 5;
+var b = 10;
+var c = 5;
+var x = (a==x);
+console.log(x)
+
+
+inequality
+var a = 5;
+var b = 10;
+var c = 5;
+var x = (a !=b);
+console.log(x)
+
+
+graterthan
+var a = 5;
+var b = 10;
+var c = 5;
+var x = (a>b);
+console.log(x)
+
+lessthan
+var a = 5;
+var b = 10;
+var c = 5;
+var x = (a<b);
+console.log(x)
+
+greaterthanorequalto
+var a = 5;
+var b = 10;
+var c = 5;
+var x = (a>=b);
+console.log(x)
+
+lessthanorequalto
+var a = 5;
+var b = 10;
+var c = 5;
+var x = (a<=b);
+console.log(x)
+
+
+
